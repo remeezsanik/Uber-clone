f005aa5f52ca04f408e6761b77a7f845ae744f18
REMEEZ SANIK <remeezsanik@RMZ-MacBook-Air.local>
2021-10-30 15:34:14 +0530

    First Commit

---
+++ screens/EatsScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, Text } from 'react-native'
+import tw from 'tailwind-react-native-classnames'
+
+const EatsScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text style={tw`text-center font-semibold`}>Coming Soon..</Text>
+        </SafeAreaView>
+    )
+}
+
+export default EatsScreen
+
+const styles = StyleSheet.create({})
